c0a4ed256135598f42ec662e3b57c3a117cb1985
Andrey Zaytsev <monolith195@gmail.com>
2019-04-27 10:15:31 +0300

    Homework 1

---
+++ lesson01/task3.py
@@ -0,0 +1,6 @@
+# 3. Определить, какие из слов «attribute», «класс», «функция», «type» невозможно записать в байтовом типе.
+
+word1 = b'attribute'
+word2 = b'класс'  # SyntaxError
+word3 = b'функция'  # SyntaxError
+word4 = b'type'
